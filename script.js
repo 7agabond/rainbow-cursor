c25a6eafd14d33c32bc194d2b33cb16fbf6d4fef
Andrew <hanato21@icloud.com>
2024-01-23 16:38:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+    let x = e.offsetX
+    let y = e.offsetY
+    cursor.style.setProperty("--x", x + "px");
+    cursor.style.setProperty("--y", y + "px");
+})
